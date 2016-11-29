8fa0b553bf8cb64f97b9b58b76d780b013b8f625
Marlene Lojan <Marlene Lojan@DESKTOP-3920IG8>
2016-11-29 14:11:18 -0500

    Richard, multiplicación

--- src/colaborativo/modelo_colaborativo.java
+++ src/colaborativo/modelo_colaborativo.java
@@ -5,10 +5,40 @@
  */
 package colaborativo;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Marlene Lojan
  */
 public class modelo_colaborativo {
+    public int n;
+    public int m;
+
+    public modelo_colaborativo(int n, int m) {
+        this.n = n;
+        this.m = m;
+    }
+
+    public int getN() {
+        return n;
+    }
+
+    public void setN(int n) {
+        this.n = n;
+    }
+
+    public int getM() {
+        return m;
+    }
+
+    public void setM(int m) {
+        this.m = m;
+    }
+    public void multiplicar(){
+        int salida;
+        salida=this.m*this.m;
+        JOptionPane.showMessageDialog(null, salida);
+    }
     
 }
